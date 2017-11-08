bc5b2120ccf194e676b81f06fcad6a5c97bac0df
Neo Ighodaro <neo@creativitykills.co>
2017-11-08 01:22:43 +0100

    code clean up

--- scripts/editor.js
+++ scripts/editor.js
@@ -3,7 +3,7 @@ const slug = document.URL.substr(document.URL.lastIndexOf('/') + 1)
 axios.get('/api/notes/'+slug).then(function (response) {
     const note = response.data.data
 
-    // Could not find that note
+    // Could not find that note, redirect to home
     if ( ! note.ID) {
         return (window.location = '/')
     }
@@ -15,7 +15,7 @@ axios.get('/api/notes/'+slug).then(function (response) {
 
     const textSync = new TextSync({ instanceLocator: "v1:us1:3ca672fc-0717-49a2-abb0-81677680b654" });
 
-    return textSync.createEditor({
+    textSync.createEditor({
         // @see https://docs.pusher.com/textsync/reference/js#editor-config-properties
         element: "#editor",
         docId: note.Slug,
@@ -24,9 +24,10 @@ axios.get('/api/notes/'+slug).then(function (response) {
                 collaborators[users[key].siteId] = users[key]
             }
 
-            const count = Object.keys(collaborators).length
-            const str = count > 1 ? `${count} people are collaborating on this note.` : 'You are working alone, invite more people.'
-            $('.navbar-brand .commentary').text(str)
+            const count   = Object.keys(collaborators).length
+            const message = count > 1 ? `${count} people are collaborating on this note.` : 'You are working alone, invite more people.'
+
+            $('.navbar-brand .commentary').text(message)
         }
     })
 })
